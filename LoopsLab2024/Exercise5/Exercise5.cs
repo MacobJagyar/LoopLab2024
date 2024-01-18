a1ea01db99ff4b9d858638f456ae2723c596361d
MacobJagyar <jacobmagyar95@gmail.com>
2024-01-18 16:24:42 -0500

    Added exercise 5 in it's own program file for clarity

---
+++ LoopsLab2024/Exercise5/Exercise5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsLab2024.Exercise5
+{
+    //repeat the same exercise as the previous,
+    //except this time implement it with a do while loop.
+
+
+    //bool locked = true;
+    //int attempts = 0;
+
+    //do
+    //{
+    //    Console.Write("Please enter the door code: ");
+    //    if (int.TryParse(Console.ReadLine(), out int userInput))
+    //    {
+    //        attempts++;
+
+    //        if (userInput == 13579)
+    //        {
+    //            Console.WriteLine("Correct. Welcome home");
+    //            locked = false;
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Incorrect. " + attempts + " attempts now used. Lock out after 5 attempts");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        attempts++;
+    //        Console.WriteLine("Invalid input. " + attempts + " attempts now used. Lock out after 5 attempts");
+
+    //    }
+
+    //    if (attempts == 5)
+    //    {
+    //        Console.WriteLine("5 ATTEMPTS HAVE BEEN TRIED. LOCK OUT.");
+    //        break;
+
+    //    }
+
+    //} while (locked == true);
+}
